83467e80a184136640b93c5c20db21c33cb1a0e7
Eduardo Reis <eduardoluisreis@gmail.com>
2023-06-14 10:35:36 -0300

    Correção de semnatica da classe

--- src/components/Registration/PlantRegistration.jsx
+++ src/components/Registration/PlantRegistration.jsx
@@ -1,3 +1,15 @@
-<>
- <form action=""></form>
-</>
+function PlantRegistration(){
+    return(
+
+ <div className="">
+    <form action="">
+    <div><input type="text" placeholder="Plant Name" /></div>
+    <div><input type="text" placeholder="Plant Type" /></div>
+    
+    </form>
+ </div>
+
+    )
+}
+
+export default PlantRegistration;
