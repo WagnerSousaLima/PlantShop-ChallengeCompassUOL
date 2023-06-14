dc5fd600a3ee000b374c32a100ed8aac0bd4a2d9
Eduardo Reis <eduardoluisreis@gmail.com>
2023-06-14 12:59:35 -0300

    Estrutura e Estilização Iniciais

--- src/components/Registration/PlantRegistration.jsx
+++ src/components/Registration/PlantRegistration.jsx
@@ -1,14 +1,38 @@
-function PlantRegistration(){
-    return(
-<>
- <form className="">
-    <h1>Plant registration</h1>
-    <div><input type="text" placeholder="Plant Name" /></div>
-    <div><input type="text" placeholder="Plant Type" /></div>  
+import "../Registration/PlantResgistration.module.css";
 
- </form>
-</>
-    )
+function PlantRegistration() {
+	return (
+		<div>
+			<form className=" register">
+				<h1>Plant registration</h1>
+                <hr/>
+
+                <label htmlFor="">Plant name</label>
+				<input type="text" placeholder="Plant name" />
+
+                <label htmlFor="">Plant subtitle</label>
+				<input type="text" placeholder="Plant type" />
+                
+                <label htmlFor="">Price</label>
+				<input type="number" placeholder="Price" />
+
+                <label htmlFor="">Discount percentage</label>
+				<input type="number" placeholder="Discount percentage" />
+
+				<div>
+                <label className="Label">Label:</label>
+				<input type="radio" value="Label" name="label" /> indoor
+				<input type="radio" value="Label" name="label" /> Outdoor
+                </div>
+
+                <label htmlFor="Label">Features:</label>
+                <textarea name="" id="" cols="30" rows="10"></textarea><br />
+
+                <label htmlFor="Label">Description:</label>
+                <textarea name="" id="" cols="30" rows="10"></textarea>
+			</form>
+		</div>
+	);
 }
 
 export default PlantRegistration;
