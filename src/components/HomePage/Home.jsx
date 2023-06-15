1b093163e54a5d78c413765d63a59faf137e0715
amanda nascimento <apmnsilva@gmail.com>
2023-06-15 16:03:19 -0300

    implemented grid on info section
    implemented grid instead of flex for a better look on the second section and also fixed another minor issues

--- src/components/HomePage/Home.jsx
+++ src/components/HomePage/Home.jsx
@@ -4,31 +4,28 @@ import leftBackground from "./Images/left plant.png";
 import nutrientsIcon from "./Images/Property 1=bag.svg";
 import wateringIcon from "./Images/Property 1=water.svg";
 import sunlightIcon from "./Images/Property 1=sun.svg";
-import firstPlant from "./Images/Rectangle 32.png";
-import secondPlant from "./Images/Rectangle 33.png";
-import thirdPlant from "./Images/Rectangle 34.png";
+import firstImg from "./Images/Rectangle 32.png";
+import secondImg from "./Images/Rectangle 33.png";
+import thirdImg from "./Images/Rectangle 34.png";
 
 const Home = () => {
   return (
     <>
       <div className={classes.homeMain}>
-        
         <div className={classes.mainContent}>
           <span className={classes.homeMainText}>
             <article className={classes.subtitle}>
-              <p>
-                <svg
-                  className={classes.rectangle}
-                  width="32"
-                  height="3"
-                  viewBox="0 0 32 3"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <rect width="32" height="3" fill="#1E1E24" />
-                </svg>
-                Love for Nature
-              </p>
+              <svg
+                className={classes.rectangle}
+                width="32"
+                height="3"
+                viewBox="0 0 32 3"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <rect width="32" height="3" fill="#1E1E24" />
+              </svg>
+              <p className={classes.subtitleText}>Love for Nature</p>
             </article>
             <h1>
               Discover your <em>green</em> side
@@ -60,8 +57,8 @@ const Home = () => {
         </span>
       </div>
 
-      <section className={classes.homeSection}>
-        <span className={classes.stepsTitle}>
+      <section className={classes.moreInfo}>
+        <span className={classes.titleItem}>
           <h2>
             Steps to take care of your <em>plants</em>
           </h2>
@@ -71,57 +68,54 @@ const Home = () => {
             way to maintaining healthy and thriving plants.
           </p>
         </span>
-        <article className={classes.stepsSection}>
-          <div>
-            <img src={wateringIcon} />
-            <h3>Watering</h3>
-            <p>
-              Water your plants when the top inch of soil feels dry to the
-              touch. Avoid overwatering, as it can lead to root dehydration.
-            </p>
-          </div>
-          <div>
-            <img src={sunlightIcon} />
-            <h3>Sunlight</h3>
-            <p>
-              Most plants need adequate sunlight to thrive. Place your plants in
-              areas that receive the appropriate amount of light for their
-              specific needs
-            </p>
-          </div>
-          <div>
-            <img src={nutrientsIcon} />
-            <h3>Nutrients and Fertilizing</h3>
-            <p>
-              Choose a suitable fertilizer based on the specific needs of your
-              plants, whether it's a balanced or specialized formula.
-            </p>
-          </div>
-        </article>
-        <article className={classes.websitePictures}>
-          <img src={firstPlant} draggable="false" />
-          <img src={secondPlant} draggable="false" />
-          <div className={classes.websiteInfo}>
-            <img src={thirdPlant} draggable="false" />
-            <p>
-              Our website offers a wide array of stunning plants, ranging from
-              vibrant flowers to lush indoor foliage, allowing you to create
-              your very own green oasis. In addition to our extensive plant
-              selection, we also provide gardening kits and fertilizers to equip
-              you with everything you need to nurture your plants and achieve
-              gardening success. But we don't stop there! We believe that
-              knowledge is the key to a thriving garden, so we offer a wealth of
-              information and resources on gardening techniques, plant care
-              tips, and landscaping ideas. Whether you're a seasoned gardener or
-              just starting your green journey, our goal is to inspire and
-              support you every step of the way. Get ready to explore our
-              virtual garden and discover the joys of gardening with us!
-            </p>
-            <button className={classes.seeMoreButton}>
-              <span className={classes.buttonText}>See more photos</span>
-            </button>
-          </div>
-        </article>
+        <div className={classes.firstStep}>
+          <img src={wateringIcon} />
+          <h3>Watering</h3>
+          <p>
+            Water your plants when the top inch of soil feels dry to the touch.
+            Avoid overwatering, as it can lead to root dehydration.
+          </p>
+        </div>
+        <div className={classes.secondStep}>
+          <img src={sunlightIcon} />
+          <h3>Sunlight</h3>
+          <p>
+            Most plants need adequate sunlight to thrive. Place your plants in
+            areas that receive the appropriate amount of light for their
+            specific needs
+          </p>
+        </div>
+        <div className={classes.thirdStep}>
+          <img src={nutrientsIcon} />
+          <h3>Nutrients and Fertilizing</h3>
+          <p>
+            Choose a suitable fertilizer based on the specific needs of your
+            plants, whether it's a balanced or specialized formula.
+          </p>
+        </div>
+
+        <img src={firstImg} className={classes.firstImg} draggable="false" />
+        <img src={secondImg} className={classes.secondImg} draggable="false" />
+        <div className={classes.websiteInfo}>
+          <img src={thirdImg} className={classes.thirdImg} draggable="false" />
+          <p>
+            Our website offers a wide array of stunning plants, ranging from
+            vibrant flowers to lush indoor foliage, allowing you to create your
+            very own green oasis. In addition to our extensive plant selection,
+            we also provide gardening kits and fertilizers to equip you with
+            everything you need to nurture your plants and achieve gardening
+            success. But we don't stop there! We believe that knowledge is the
+            key to a thriving garden, so we offer a wealth of information and
+            resources on gardening techniques, plant care tips, and landscaping
+            ideas. Whether you're a seasoned gardener or just starting your
+            green journey, our goal is to inspire and support you every step of
+            the way. Get ready to explore our virtual garden and discover the
+            joys of gardening with us!
+          </p>
+          <button className={classes.seeMoreButton}>
+            <span className={classes.buttonText}>See more photos</span>
+          </button>
+        </div>
       </section>
     </>
   );
