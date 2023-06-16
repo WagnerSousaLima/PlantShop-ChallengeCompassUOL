9dfb72ef8bee9c610b15f99348eaa44db0d3c5ce
Wagner Lima <wagner.sousalima@gmail.com>
2023-06-16 16:26:45 -0300

    alteração de logica de renderizaão da pagina, instalação de bibliotecas e ajuster no css

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,45 @@
-
 import React from 'react';
 import { Header } from './components/Header/Header';
 import './global.css';
-
-import { ClerkProvider } from "@clerk/clerk-react";
-
-
-if (!import.meta.env.VITE_REACT_APP_CLERK_PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key")
-}
-const clerkPubKey = import.meta.env.VITE_REACT_APP_CLERK_PUBLISHABLE_KEY;
+import { ClerkProvider, useUser, RedirectToSignIn } from '@clerk/clerk-react';
+import { neobrutalism } from '@clerk/themes';
 
 function App() {
- 
+  const clerkPubKey = import.meta.env.VITE_REACT_APP_CLERK_PUBLISHABLE_KEY;
+
   return (
-    <>
-      <ClerkProvider publishableKey={clerkPubKey}> 
-
-          <Header />
-
+    <div>
+      <ClerkProvider
+        publishableKey={clerkPubKey}
+        appearance={{
+          baseTheme: neobrutalism,
+          elements: { formButtonPrimary: 'bg-green-500 hover:bg-green-600' }
+        }}
+      >
+        <AuthWrapper />
       </ClerkProvider>
-     
-      
-    </>
-  )
+      {/* Restante do conteúdo do aplicativo */}
+    </div>
+  );
 }
 
-export default App
+function AuthWrapper() {
+  const { user, isLoading } = useUser();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (user) {
+    return (
+      <>
+        <Header />
+        {/* Restante do conteúdo quando o usuário estiver logado */}
+      </>
+    );
+  } else {
+    return <RedirectToSignIn />;
+  }
+}
+
+export default App;
