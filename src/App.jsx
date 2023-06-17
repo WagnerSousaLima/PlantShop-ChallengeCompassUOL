c02a84a34c30338dab0fd647f31451fc3c90c134
Nicacio Basilio <nicacioufrn@gmail.com>
2023-06-16 22:23:39 -0300

    finalizing routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,46 @@
-import './global.css';
+import React from 'react';
 import Footer from './components/Footer/Footer';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './global.css';
 
-function App() {
-
+const Home = () => {
   return (
     <>
-
-      <Header />
-
-      <Home />
-
-      <CarouselCards />
-
+      <h1>Página Inicial</h1>
       <Footer />
-
     </>
-  )
+  );
+};
+
+const Product = () => {
+  return (
+    <>
+      <h2>Produtos</h2>
+      <div>Insira os componentes dos produtos aqui</div>
+      <Footer />
+    </>
+  );
+};
+
+const Registration = () => {
+  return (
+    <>
+      <h2>Registro</h2>
+      <div>Insira os componentes do registro aqui</div>
+    </>
+  );
+};
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/registration" element={<Registration />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,23 +1,43 @@
 import React from "react";
+import { Helmet } from "react-helmet";
+import styles from "./Footer.module.css";
 
 const Footer = (props) => {
   return (
-    <footer>
-        <div className="main">
-
+    <footer className={styles.footer}>
+      <Helmet>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=EB+Garamond:wght@700&family=Lato:wght@700&family=Raleway&display=swap"
+          rel="stylesheet"
+        />
+      </Helmet>
+      <div className={styles.top}>
+        <div className={styles.main}>
+          <h1>Stay Fresh</h1>
+          <a href="#">compassinhos@gmail.com</a>
+          <a href="#">+55 41 99999-9999</a>
         </div>
-        <div className="aside">
+        <div className={styles.aside}>
           <div className="links">
-            <h1>Links</h1>
+            <h2>Links</h2>
+            <a href="#">About us</a>
+            <a href="#">Products</a>
+            <a href="#">Blogs</a>
           </div>
           <div className="community">
-            
+          <h2>Community</h2>
+            <a href="#">About us</a>
+            <a href="#">Products</a>
+            <a href="#">Blogs</a>
           </div>
         </div>
-        <div className="base">
-
-        </div>
-  </footer>
+      </div>
+      <div className={styles.base}>
+        <img src="../../../public/images/footer_logo.png" alt="logo" />
+      </div>
+    </footer>
   )
 }
 
