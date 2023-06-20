9504c726470d910d4ed0bb0601955f04c572cd80
Wagner Lima <wagner.sousalima@gmail.com>
2023-06-20 03:03:28 -0300

    Page Products refatorada e corrigida

--- src/components/AllProducts/AllProducts.jsx
+++ src/components/AllProducts/AllProducts.jsx
@@ -1,53 +1,120 @@
 import React, { useEffect, useState } from "react";
 import CardPlant from "../HomeCarousel/Card/CardPlant";
-import { api } from '../../Services/api';
-import { FilterByType } from "./FilterByType";
+import { api } from "../../Services/api";
 import styles from "./AllProducts.module.css";
 
-
 export function AllProducts() {
-  const [selectedButton, setSelectedButton] = useState("all");
+  const [selectedButton, setSelectedButton] = useState("order by");
   const [plants, setPlants] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [sortBy, setSortBy] = useState("A-Z");
+  const [hoveredButton, setHoveredButton] = useState(null);
 
   useEffect(() => {
-    api.get('./plants').then((response) => {
+    api.get("/plants").then((response) => {
       setPlants(response.data);
+      setFilteredProducts(response.data);
     });
   }, []);
 
-  const handleButtonClick = (button) => {
-    setSelectedButton(button);
+  useEffect(() => {
+    let sortedProducts = [...plants];
+
+    if (sortBy === "A-Z") {
+      sortedProducts = sortedProducts.sort((a, b) =>
+        a.name.localeCompare(b.name)
+      );
+    } else if (sortBy === "Z-A") {
+      sortedProducts = sortedProducts.sort((a, b) =>
+        b.name.localeCompare(a.name)
+      );
+    } else if (sortBy === "Maior Preço") {
+      sortedProducts = sortedProducts.sort((a, b) => b.price - a.price);
+    } else if (sortBy === "Menor Preço") {
+      sortedProducts = sortedProducts.sort((a, b) => a.price - b.price);
+    }
+
+    setFilteredProducts(sortedProducts);
+  }, [sortBy, plants]);
+
+  const handleSortBy = (option) => {
+    setSortBy(option);
+    setHoveredButton(null);
   };
 
-  const filteredProducts = plants.filter((plant) => {
-    if (selectedButton === "best sellers") {
-      return !plant.discount || plant.discount === 0;
-    } else if (selectedButton === "offers") {
-      return plant.discount > 0;
-    } else {
-      return true; // Mostrar todos os produtos
-    }
-  });
-  
+  const handleHover = (option) => {
+    setHoveredButton(option);
+  };
+
+  const handleLeave = () => {
+    setHoveredButton(null);
+  };
+
+  const isSelectedButton = (button) => {
+    return selectedButton === button ? styles.selected : "";
+  };
 
   return (
-    <>
-      <FilterByType
-        selected={selectedButton}
-        handleButtonClick={handleButtonClick}
-      />
-
-      <div className={styles.cardGrid}> {/* Container da grade */}
-        {filteredProducts.map((plants) => (
+    <div className={styles.allProducts}>
+      <div className={styles.filterContainer}>
+        <button
+          className={`${styles.filterButton} ${selectedButton === "A-Z" ? styles.selected : ""}`}
+          onClick={() => setSelectedButton("order by")}
+          onMouseEnter={() => handleHover("order by")}
+          onMouseLeave={handleLeave}
+        >
+          Order by your preferences:
+        </button>
+        {selectedButton === "order by" && (
+          <div className={styles.filterButtonContainer}>
+            <button
+              className={`${styles.filterButton} ${isSelectedButton("A-Z")} ${hoveredButton === "A-Z" ? styles.hovered : ""}`}
+              onClick={() => handleSortBy("A-Z")}
+              onMouseEnter={() => handleHover("A-Z")}
+              onMouseLeave={handleLeave}
+            >
+              Sort A-Z
+            </button>
+            <button
+              className={`${styles.filterButton} ${isSelectedButton("Z-A")} ${hoveredButton === "Z-A" ? styles.hovered : ""}`}
+              onClick={() => handleSortBy("Z-A")}
+              onMouseEnter={() => handleHover("Z-A")}
+              onMouseLeave={handleLeave}
+            >
+              Sort Z-A
+            </button>
+            <button
+              className={`${styles.filterButton} ${isSelectedButton("Maior Preço")} ${hoveredButton === "Maior Preço" ? styles.hovered : ""}`}
+              onClick={() => handleSortBy("Maior Preço")}
+              onMouseEnter={() => handleHover("Maior Preço")}
+              onMouseLeave={handleLeave}
+            >
+              Sort Price (High to Low)
+            </button>
+            <button
+              className={`${styles.filterButton} ${isSelectedButton("Menor Preço")} ${hoveredButton === "Menor Preço" ? styles.hovered : ""}`}
+              onClick={() => handleSortBy("Menor Preço")}
+              onMouseEnter={() => handleHover("Menor Preço")}
+              onMouseLeave={handleLeave}
+            >
+              Sort Price (Low to High)
+            </button>
+          </div>
+        )}
+      </div>
+      <div className={styles.cardGrid}>
+        {filteredProducts.map((plant) => (
           <CardPlant
-            key={plants.id}
-            name={plants.name}
-            price={plants.price}
-            discount={plants.discount}
-            label={plants.label}
+            key={plant.id}
+            name={plant.name}
+            price={plant.price}
+            discount={plant.discount}
+            label={plant.label}
           />
         ))}
+
+        <footer />
       </div>
-    </>
+    </div>
   );
 }
