6dbc305c14dfe71203de181e65ff4761a7b1c3c7
unknown <gpereirac2001@gmail.com>
2023-06-20 14:06:24 -0300

    Adding the route to details page

--- src/components/HomeCarousel/Card/CardPlant.jsx
+++ src/components/HomeCarousel/Card/CardPlant.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import style from './CardPlant.module.css';
 import cacto from './imgs/cacto.png'
 
@@ -5,21 +6,23 @@ const CardPlant = (props) => {
     const discount = props.price - ((props.discount / 100) * props.price);
     
     return(
-        <div className={style.divCard}>
-            <img className={style.imageCard} src={cacto} alt="Foto de um Cacto"/>
-            <div className={style.plantInfos}>
-                <p className={style.plantTitle}>{props.name}</p>
-                <div>
-                    {props.discount > 0 ? 
-                        <div>
-                            <span className={style.plantPrice}>${discount.toFixed(2)}</span>
-                            <span className={style.plantDiscount}>${props.price}</span>
-                        </div>
-                    : <span className={style.plantPrice}>${props.price}</span>}
+        <Link to={`/Product/${props.id}`}>
+            <div className={style.divCard}>
+                <img className={style.imageCard} src={cacto} alt="Foto de um Cacto"/>
+                <div className={style.plantInfos}>
+                    <p className={style.plantTitle}>{props.name}</p>
+                    <div>
+                        {props.discount > 0 ? 
+                            <div>
+                                <span className={style.plantPrice}>${discount.toFixed(2)}</span>
+                                <span className={style.plantDiscount}>${props.price}</span>
+                            </div>
+                        : <span className={style.plantPrice}>${props.price}</span>}
+                    </div>
+                    <label className={style.plantTag}>{props.label[0]}</label>
                 </div>
-                <label className={style.plantTag}>{props.label[0]}</label>
             </div>
-        </div>
+         </Link>
     )
 }
 
