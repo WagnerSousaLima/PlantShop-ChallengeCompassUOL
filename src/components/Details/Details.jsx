2632f1286c9da23350c064ef920964cbb8f82ed5
Tita-Simoni <cintiasimoni.vet@gmail.com>
2023-06-21 14:36:08 -0300

    Adjustments of AboutUs and Details

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,18 +1,13 @@
 import React from 'react';
-
 import { plants } from '../../../server.json';
-
 import { useParams } from 'react-router-dom';
-
 import styles from './Details.module.css';
 
 const Details = () => {
     const parametros = useParams();
-
     const props = plants.find((plant) => plant.id === Number(parametros.id));
 
-    const discount = props.price - ((props.discountPercentage / 100) * props.price);
-        
+    const discount = props.price - ((props.discountPercentage / 100) * props.price);        
     const handleClick = () => {
         window.location.href = '/registration';
     }
@@ -23,35 +18,33 @@ const Details = () => {
                 alt="Echinocereus Cactus" 
                 className={styles.image}
             />
-            <div className={styles.content}>
-             
-
-                <h1>{props.name}</h1>
+            <div className={styles.content}>        
+                <h1 className={styles.title}>{props.name}</h1>
                 <span className={styles.title_description}>{props.subtitle}</span>
                 {props.label.length > 1 ?
                     <div className={styles.tags}>
                         <p className={styles.tag}>{props.label[0]}</p>
                         <p className={styles.tag}>{props.label[1]}</p>
                     </div> 
-                : <div className={styles.tags}>
-                    <p className={styles.tag}>{props.label[0]}</p>
-                </div> 
+                :   <div className={styles.tags}>
+                        <p className={styles.tag}>{props.label[0]}</p>
+                    </div> 
                 }
                 {props.discountPercentage > 0 ? 
-                    <div>
+                    <div className={styles.price}>
                         <p className={styles.price}>${discount.toFixed(2)}</p>
                         <p className={styles.price}>${props.price}</p>
                     </div>
                     : <span className={styles.price}>${props.price}</span>
                 }                
                 
-                    <button onClick={handleClick} className={styles.button}>Check out</button>
-                
-                <h2>Features</h2>
+                <button onClick={handleClick} className={styles.button}>Check out</button>
+
+                <h2 className={styles.subtitle}>Features</h2>
                 <ul className={styles.features}>
-                    <li>{props.features}</li>           
+                    <li className={styles.features}>{props.features}</li>           
                 </ul>
-                <h2>Description</h2>
+                <h2 className={styles.subtitle}>Description</h2>
                 <p className={styles.description}>{props.description}</p>                
             </div>
         </div>
